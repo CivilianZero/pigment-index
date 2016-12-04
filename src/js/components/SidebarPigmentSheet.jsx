dcd8f52c415fa5dd7777bac2e3a25dd65ce21a7e
Matt Catoe <catoemw@gmail.com>
2016-12-04 16:42:19 -0500

    fixed sidebar styling

--- src/js/components/SidebarPigmentSheet.jsx
+++ src/js/components/SidebarPigmentSheet.jsx
@@ -9,11 +9,11 @@ var SidebarPigmentSheet = React.createClass({
 
 	render() {
 		var p = this.state.pigment,
-			etymology = p.name.etym !== '' ? <li><span>Etymology:</span>{p.name.etym}</li> : null,
-			altName = p.name.altNames !== '' ? <li><span>Other Names:</span>{p.name.altNames}</li> : null,
-			source = p.origins.source !== '' ? <li><span>Source:</span>{p.origins.source}</li> : null,
-			useStart = p.origins.useStart !== '' ? <li><span>Discovery:</span>{p.origins.useStart}</li> : null,
-			useEnd = p.origins.useEnd !== '' ? <li><span>End of use:</span>{p.origins.useEnd}</li> : null,
+			etymology = p.name.etym !== '' ? <li><span>Etymology:</span> {p.name.etym}</li> : null,
+			altName = p.name.altNames !== '' ? <li><span>Other Names:</span> {p.name.altNames}</li> : null,
+			source = p.origins.source !== '' ? <li><span>Source:</span> {p.origins.source}</li> : null,
+			useStart = p.origins.useStart !== '' ? <li><span>Discovery:</span> {p.origins.useStart}</li> : null,
+			useEnd = p.origins.useEnd !== '' ? <li><span>End of use:</span> {p.origins.useEnd}</li> : null,
 			style = p.name.common === 'Lead white' ? {
 				'backgroundColor': p.images.primary, 
 				'border': '1px solid black'
@@ -24,9 +24,9 @@ var SidebarPigmentSheet = React.createClass({
 
 		return(
 			<section className='sidebar-sheet'>
-				<div style={style} className='sidebar-color'></div>
 				<section className='basic-info'>
 					<h1>{p.name.common}</h1>
+					<div style={style} className='sidebar-color'></div>
 					<ul>
 						{etymology}
 						{altName}
