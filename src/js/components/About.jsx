3de04b3390455e6d96db5e38b6d1f123fd7e73b1
Matt Catoe <catoemw@gmail.com>
2016-12-06 13:38:02 -0500

    updated about content and timeline buttons

--- src/js/components/About.jsx
+++ src/js/components/About.jsx
@@ -3,7 +3,19 @@ var React = require('react');
 var About = React.createClass({
 
 	render() {
-		return <p>This is About</p>
+		return <section className='about'>
+					<h1>References</h1>
+					<p>Gottsegen, Mark David. The Painter's Handbook. New York: Watson-Guptill Publications, 2006. Print.</p>
+					<p>Seymour, Pip. The Artist's Handbook. London: Arcturus, 2003. Print.</p>
+					<p>Mayer, Ralph. The Artist's Handbook of Materials and Techniques. 5th ed. New York: Penguin Group, 1991. Print.</p>
+					<h1>Images</h1>
+					<p>colormaker.com | history.org | mkarma.blogspot.com | geology.com | cds.periodictable.files.wordpress.com | coulourlex.com | 
+					windsornewton.com | curtisward.com | johnbetts-fineminerals.com | masterpigments.com | factfile.org | insituconservation.com | 
+					theveganplanet.net | thedomesticsoundscape.com | forestpoppy.com | besttourism.com | scents-of-earth.com | 
+					earthpigments.com | angloamerican.com | medieval.colab.duke.edu | globalsources.com | azulbarrientos.com |
+					nationalgallery.org.uk | chemistry.stackexchange.com | blogs.bl.uk | mineral-forum.com | wetcanvas.com | dailymail.co.uk | 
+					paleotechnics.wordpress.com | magazine.harvardartmuseums.org</p>
+				</section>
 	}
 });
 
